3f3f43925524392d54692f0bda88d0d78e71ed6f
Raigen93 <101114493+Raigen93@users.noreply.github.com>
2022-10-29 21:15:45 -0400

    update logic/ work on die roll method

--- logic.js
+++ logic.js
@@ -11,14 +11,31 @@
         chaAdjust = 0,
             baseCha = 8,
         rollStr,
+            strBuff,
         rollDex,
+            dexBuff,
         rollCon,
+            conBuff,
         rollInt,
+            intBuff,
         rollWis,
+            wisBuff,
         rollCha,
+            chaBuff,
            pointBuy = 32;
 
     let pointValue;
+    let raceBuff = document.getElementById('raceBuff');
+
+    function resetBuffs() {
+        strBuff = '';
+        dexBuff = '';
+        conBuff = '';
+        intBuff = '';
+        wisBuff = '';
+        chaBuff = '';
+        raceBuff.innerText = '';
+    }
 
     function resetVals() {
             strAdjust = 0,
@@ -36,6 +53,7 @@
             pointBuy = 32;
     }
 
+
     function setBases() {
         document.getElementById('playerChoice').style.display = 'none';
         document.getElementById('playerChoice2').style.display = 'none';
@@ -43,103 +61,189 @@
         let race = document.getElementById("race").selectedIndex;
         let raceArr = document.querySelectorAll('.option1');
         let use = raceArr[race].value;
-        let raceBuff = document.getElementById('raceBuff');
 
-       resetVals();
 
-        if(use === 'reset'){
+        resetVals();
+        resetBuffs();
+
+        if (use === 'reset') {
             resetVals();
+            resetBuffs();
+            raceBuff.innerText = '';
 
-        } else if(use === 'Aarakocra') {
+        } else if (use === 'Aarakocra') {
             baseDex += 2;
             baseWis += 1;
+
+            dexBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Dexterity and +1 to Wisdom';
-        } else if(use === 'Fallen Aasimar') {
+
+        } else if (use === 'Fallen Aasimar') {
             baseCha += 2;
             baseStr += 1;
+
+            chaBuff = 2;
+            strBuff = 1;
             raceBuff.innerText = 'You receive +2 to Charisma and +1 to Strength';
-        } else if(use === 'Protector Aasimar') {
+
+        } else if (use === 'Protector Aasimar') {
             baseCha += 2;
             baseWis += 1;
+
+            chaBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Charisma and +1 to Wisdom';
-        } else if(use === 'Scourge Aasimar') {
+
+        } else if (use === 'Scourge Aasimar') {
             baseCha += 2;
             baseCon += 2;
+
+            chaBuff = 2;
+            conBuff = 2;
             raceBuff.innerText = 'You receive +2 to Charisma and +2 to Constitution';
-        } else if(use === 'Variant Aasimar') {
+
+        } else if (use === 'Variant Aasimar') {
             baseCha += 2;
             baseWis += 1;
+
+            chaBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Charisma and +1 to Wisdom';
-        } else if(use === 'Bugbear') {
+
+        } else if (use === 'Bugbear') {
             baseStr += 2;
             baseDex += 1;
+
+            strBuff = 2;
+            dexBuff = 1
             raceBuff.innerText = 'You receive +2 to Strength and +1 to Dexterity';
-        } else if(use === 'Centaur') {
+
+        } else if (use === 'Centaur') {
             baseStr += 2;
             baseWis += 1;
+
+            strBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Strength and +1 to Wisdom';
-        } else if(use === 'Dragonborn') {
+
+        } else if (use === 'Dragonborn') {
             baseStr += 2;
             baseCha += 1;
+
+            strBuff = 2;
+            chaBuff = 1;
             raceBuff.innerText = 'You receive +2 to Strength and +1 to Charisma';
-        } else if(use === 'Hill Dwarf') {
+
+        } else if (use === 'Hill Dwarf') {
             baseCon += 2;
             baseWis += 1;
+
+            conBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Constitution and +1 to Wisdom';
-        } else if(use === 'Mountain Dwarf') {
+
+        } else if (use === 'Mountain Dwarf') {
             baseCon += 2;
             baseStr += 2;
+
+            conBuff = 2;
+            strBuff = 2;
             raceBuff.innerText = 'You receive +2 to Constitution and +2 to Strength';
-        } else if(use === 'High Elf') {
+
+        } else if (use === 'High Elf') {
             baseDex += 2;
             baseInt += 1;
+
+            dexBuff = 2;
+            intBuff = 1;
             raceBuff.innerText = 'You receive +2 to Dexterity and +1 to Intelligence';
-        } else if(use === 'Wood Elf') {
+
+        } else if (use === 'Wood Elf') {
             baseDex += 2;
             baseWis += 1;
+
+            dexBuff = 2;
+            wisBuff = 1;
             raceBuff.innerText = 'You receive +2 to Dexterity and +1 to Wisdom';
-        } else if(use === 'Half-Elf') {
+
+        } else if (use === 'Half-Elf') {
             baseCha += 2;
             document.getElementById('playerChoice').style.display = 'inline-block';
+
+            chaBuff = 2;
             raceBuff.innerText = 'You receive +2 to Charisma and +1 to a stat of your choosing';
-        } else if(use === 'Half-Orc') {
+
+        } else if (use === 'Half-Orc') {
             baseStr += 2;
             baseCon += 1;
+
+            strBuff = 2;
+            conBuff = 1;
             raceBuff.innerText = 'You receive +2 to Strength and +1 to Constitution';
-        } else if(use === 'LightFoot Halfling') {
+
+        } else if (use === 'LightFoot Halfling') {
             baseDex += 2;
             baseCha += 1;
+
+            dexBuff = 2;
+            chaBuff = 1;
             raceBuff.innerText = 'You receive +2 to Dexterity and +1 to Charisma';
-        } else if(use === 'Stout Halfling') {
+
+        } else if (use === 'Stout Halfling') {
             baseDex += 2;
             baseCon += 1;
+
+            dexBuff = 2;
+            conBuff = 1;
             raceBuff.innerText = 'You receive +2 to Dexterity and +1 to Constitution';
-        } else if(use === 'Human') {
+
+        } else if (use === 'Human') {
             baseCha += 1;
             baseWis += 1;
             baseDex += 1;
             baseStr += 1;
             baseCon += 1;
             baseInt += 1;
+
+            strBuff = 1;
+            dexBuff = 1;
+            conBuff = 1;
+            intBuff = 1;
+            wisBuff = 1;
+            chaBuff = 1;
             raceBuff.innerText = 'You receive +1 to all stats';
-        } else if(use === 'Human Variant') {
+
+        } else if (use === 'Human Variant') {
             document.getElementById('playerChoice').style.display = 'inline-block';
             document.getElementById('playerChoice2').style.display = 'inline-block';
+
             raceBuff.innerText = 'You can choose two different stats to increase by +1';
-        } else if(use === 'Rock Gnome') {
+
+        } else if (use === 'Rock Gnome') {
             baseInt += 2;
             baseCon += 1;
+
+            intBuff = 2;
+            conBuff = 1;
             raceBuff.innerText = 'You receive +2 to Intelligence and +1 to Constitution';
-        } else if(use === 'Tiefling') {
+
+        } else if (use === 'Tiefling') {
             baseInt += 1;
             baseCha += 2;
+
+            intBuff = 1;
+            chaBuff = 2;
             raceBuff.innerText = 'You receive +2 to Charisma and +1 to Intelligence';
+
         }
+
+        updateBuffs();
+
     }
 
    function statDecrease(stat, baseStat) {
        let total = baseStat + stat;
-
 
        if(total >= 19) {
            pointValue = 4;
@@ -150,7 +254,6 @@
        } else if(total <= 14) {
            pointValue = 1;
        }
-        console.log(pointBuy);
        if((baseStat + stat) - 1 >= 4) {
            stat = stat -1;
             pointBuyUp(pointValue);
@@ -173,7 +276,7 @@
       } else if(total >= 18) {
           pointValue = 4;
       }
-        console.log(pointBuy);
+
         if(pointBuy <= 0 || (pointBuy - pointValue) < 0) {
             throw 'No more points to use!';
         }
@@ -186,6 +289,46 @@
             throw 'Cannot raise stat any further!';
         }
 
+    }
+
+    function updateBuffs() {
+        document.getElementById('strBuff').innerText = `+${strBuff}`;
+        document.getElementById('dexBuff').innerText = `+${dexBuff}`;
+        document.getElementById('conBuff').innerText = `+${conBuff}`;
+        document.getElementById('intBuff').innerText = `+${intBuff}`;
+        document.getElementById('wisBuff').innerText = `+${wisBuff}`;
+        document.getElementById('chaBuff').innerText = `+${chaBuff}`;
+
+        if(strBuff === '') {
+            document.getElementById('strBuff').style.display = 'none';
+        } else {
+            document.getElementById('strBuff').style.display = 'flex';
+        }
+        if(dexBuff === '') {
+            document.getElementById('dexBuff').style.display = 'none';
+        } else {
+            document.getElementById('dexBuff').style.display = 'flex';
+        }
+        if(conBuff === '') {
+            document.getElementById('conBuff').style.display = 'none';
+        } else {
+            document.getElementById('conBuff').style.display = 'flex';
+        }
+        if(intBuff === '') {
+            document.getElementById('intBuff').style.display = 'none';
+        } else {
+            document.getElementById('intBuff').style.display = 'flex';
+        }
+        if(wisBuff === '') {
+            document.getElementById('wisBuff').style.display = 'none';
+        } else {
+            document.getElementById('wisBuff').style.display = 'flex';
+        }
+        if(chaBuff === '') {
+            document.getElementById('chaBuff').style.display = 'none';
+        } else {
+            document.getElementById('chaBuff').style.display = 'flex';
+        }
     }
 
    function setAdjust() {
@@ -221,32 +364,53 @@
         return pointBuy;
     }
 
+    function updateChoice() {
+        document.getElementById('pick1').selectedIndex = 0;
+        document.getElementById('pick2').selectedIndex = 0;
+    }
+
+    function resetRace() {
+        document.getElementById('race').selectedIndex = 0;
+        raceBuff.innerText = '';
+    }
+
     function playerChoice(id, selector) {
-            setBases();
 
             let selection = document.getElementById(id).selectedIndex;
             let choices = document.querySelectorAll(selector);
             let choice = choices[selection].value;
 
+            if(document.getElementById('playerChoice2').style.display === 'none' || document.getElementById('playerChoice').style.display === 'none') {
+             setBases();
+            };
             if(choice === 'str') {
                 baseStr += 1;
+                strBuff = 1;
             } else if(choice === 'dex') {
                 baseDex += 1;
+                dexBuff = 1;
             } else if(choice === 'con') {
                 baseCon += 1;
+                conBuff = 1;
             } else if(choice === 'int') {
                 baseInt += 1;
+                intBuff = 1;
             } else if(choice === 'wis') {
                 baseWis += 1;
+                wisBuff = 1;
             } else if(choice === 'cha') {
                 baseCha += 1;
+                chaBuff = 1;
             }
+
+            updateBuffs();
 
     }
 
     function updatePoints() {
         document.getElementById('pointsRemaining').innerText = `Points remaining: ${pointBuy}`
     }
+
     updatePoints();
     setAdjust();
     setBases();
@@ -298,10 +462,10 @@
     if(pointBuy.style.display !== 'none') {
          pointBuy.style.display = 'none';
          diceRoll.style.display = 'block';
-         toggle.innerText = 'Dice Roller';
+         toggle.innerText = 'Switch to Point Buy';
      } else if(pointBuy.style.display === 'none') {
          diceRoll.style.display = 'none';
          pointBuy.style.display = 'block';
-         toggle.innerText = 'Point Buy';
+         toggle.innerText = 'Switch to Dice Roller';
      }
  }
